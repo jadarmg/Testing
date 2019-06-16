73bfcc2b0ba46fcc1d0be1ffe201c6df65fc362d
jadarmg <jadar.mg@gmail.com>
2019-06-16 14:54:55 +0530

    modified

--- Testing/src/Demo.java
+++ Testing/src/Demo.java
@@ -4,7 +4,8 @@ public class Demo {
 	public static void main(String[] args) 
 	
 	{
-	System.out.println("Pushing");	// TODO Auto-generated method stub
+	System.out.println("Pushing");	
+	System.out.println("Pulling");
 
 	}
 
